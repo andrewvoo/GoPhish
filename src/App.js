7509ffbf33fe41247bbf0f9a74b78e050c63d5bf
Drew Vo <61421870+andrewvoo@users.noreply.github.com>
2023-10-10 19:26:04 -0400

    Add files via upload
    files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './parts/Navbar'
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/parts/Navbar.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react'
+import './Navbar.css'
+import {FaBars, FaTimes} from 'react-icons/fa'
+
+const Navbar = () => {
+const [click, setClick] = useState(false)
+const handleClick = () => setClick(!click)
+
+    return (
+        <div className='header'>
+            <div className='container'>
+                <h1><span className='primary'>GoPhish</span></h1>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li>
+                        <a href='/'> Home</a>
+                    </li>
+                    <li>
+                        <a href='/'> About</a>
+                    </li>
+                    <li>
+                        <a href='/'> Switch Themes</a>
+                    </li>
+                    </ul>
+                <div className='hamburger' onClick={handleClick}>
+                    {click ? (<FaTimes size={20} style={{color: '#333'}}/>) : (<FaBars size={20} style={{color: '#333'}} />)}
+                </div>
+            </div>        
+        
+    
+        </div>
+    )
+
+
+
+
+}
+
+export default Navbar
