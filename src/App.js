65fff5e4ec6f633a56da162bb0673efada9e8d32
Drew Vo <61421870+andrewvoo@users.noreply.github.com>
2023-10-14 14:14:54 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from './parts/Navbar'
+import FishLogo from './images/fishhook.jpg'
+import './index.css'
 
 function App() {
   return (
     <>
+    <div className='App'>
     <Navbar />
+
+    <img src={FishLogo} alt="logo" className='logo'/>
+    <p>Enter a URL below to check its validity. Must end in a top-level domain (.com, .gov, .co.uk, .de, etc.)</p>
+    </div>
     </>
   );
 }
